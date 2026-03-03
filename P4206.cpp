c9e6713a36ca6ee9534de45ff918e8b5ea5c0c9b
MKurisuqwq <mkurisuqwq@icloud.com>
2026-03-03 19:22:39 +0800

    26/03/03

---
+++ P4206.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+
+const int MAXN = 1e5 + 10;
+int n, m, dis[MAXN][MAXN];
+vector<pii> g[MAXN];
+
+namespace dijskra {
+    void dijskra(int s, int *dis) {}
+} // namespace dijskra
+
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+
+    cin >> n >> m;
+    for (int i = 1, u, v; i <= m; i++) {
+        cin >> u >> v;
+        g[u].emplace_back(v);
+        g[v].emplace_back(u);
+    }
+
+    return 0;
+}
