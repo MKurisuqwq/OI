2d66b059d4d2e0f3a6213372b7cb235d38e568de
MKurisuqwq <invinciblepeach@gmail.com>
2026-03-05 21:53:42 +0800

    26/03/05

--- P10217.cpp
+++ P10217.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#define int long long
 
 using namespace std;
 using ll = long long;
@@ -8,10 +9,22 @@ const int MAXN = 1e5 + 10;
 int n, k, X, Y, a[MAXN], b[MAXN], sum[MAXN], l[MAXN], r[MAXN];
 
 void solve(int dx, int dy) {
-    int pos = dx * X + dy * Y;
-    for (int i = 1; i <= n; i++) sum[i] = sum[i - 1] + dx * a[i] + dy * b[i];
+    int tmp = dx * X + dy * Y;
+    for (int i = 1; i <= n; i++) sum[i] = sum[i - 1] + dx * a[i] + dy * b[i] + k;
     if (!sum[n]) {
-        ;
+        for (int i = 1; i <= n; i++)
+            if (sum[i] < tmp) r[i] = -1;
+        return;
+    }
+    for (int i = 1; i <= n; i++) {
+        if (sum[n] > 0) {
+            if (sum[i] < tmp) l[i] = max(l[i], (tmp - sum[i] + sum[n] - 1) / sum[n]);
+        } else {
+            if (sum[i] < tmp)
+                r[i] = -1;
+            else
+                r[i] = min(r[i], (tmp - sum[i]) / sum[n]);
+        }
     }
 }
 
@@ -23,11 +36,16 @@ signed main() {
     cin >> T;
     while (T-- > 0) {
         cin >> n >> k >> X >> Y;
-        for (int i = 1; i <= n; i++) cin >> a[i] >> b[i], r[i] = INT_MAX;
+        for (int i = 1; i <= n; i++) cin >> a[i] >> b[i], l[i] = 0, r[i] = LLONG_MAX;
         if (!X && !Y) {
             cout << "0\n";
             continue;
         }
+        solve(1, 1), solve(1, -1), solve(-1, 1), solve(-1, -1);
+        int ans = LLONG_MAX;
+        for (int i = 1; i <= n; i++)
+            if (l[i] <= r[i]) ans = min(ans, l[i] * n + i);
+        cout << (ans == LLONG_MAX ? -1 : ans) << '\n';
     }
 
     return 0;
